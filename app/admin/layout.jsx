44faa2b79d9e909fd93eb01a925a98e7a4fafd2d
meprkbd <meprkbd@gmail.com>
2025-09-18 05:39:23 +0600

    first commit

---
+++ app/admin/layout.jsx
@@ -0,0 +1,14 @@
+import AdminLayout from "../../components/admin/AdminLayout";
+
+export const metadata = {
+  title: "GoCart. - Admin",
+  description: "GoCart. - Admin",
+};
+
+export default function RootAdminLayout({ children }) {
+  return (
+    <>
+      <AdminLayout>{children}</AdminLayout>
+    </>
+  );
+}
